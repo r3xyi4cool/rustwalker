6cdebd46b7b29c1f8365749fb0b33ecf027e3c99
rexyiscool <sanjaysanal3028@gmail.com>
2025-08-27 15:21:17 +0530

    Implemented the complete Caching System

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use walkdir::WalkDir;
-use std::fs::{self, metadata, File};
+use std::fs::{self, File};
 use std::{io::{self, BufReader}, path::Path};
 use std::time::{Instant, SystemTime, UNIX_EPOCH};
 use serde::{Deserialize, Serialize};
 use serde_json;
 use tempfile::NamedTempFile;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct Fileinfo{
     path:String,
     size:u64,
-    date_modifed:u64,
+    date_modified:u64,
 }
 type CacheData = Vec<Fileinfo>;
 
@@ -34,11 +34,12 @@ fn save_cache(cache_file: &str, files: &[Fileinfo]) -> io::Result<()> {
     println!("Cache updated with {} files", files.len());
     Ok(())
 }
+
 fn is_file_uptodate(cached_file : &Fileinfo) -> bool{
     let path = Path::new(&cached_file.path);
 
     match get_file_time(path) {
-        Ok(modifie) => modifie == cached_file.date_modifed,
+        Ok(modifie) => modifie == cached_file.date_modified,
         Err(_) => false,
     }
 } 
@@ -47,7 +48,16 @@ fn walk(path:&str,min_size: u64,cache_file: &str) {
     let mut files_scanned = 0;
     let mut permission_denied = 0;
     let mut other_errors = 0;
-    let mut found_files: Vec<(std::path::PathBuf, u64)> = Vec::new();
+
+    let cache = load_cache(cache_file).unwrap_or_else(|e|{
+        println!("No cache found ({}) creating a new vector",e);
+        Vec::new()
+    });
+
+    let mut cachemap: std::collections::HashMap<String,Fileinfo> = cache.into_iter().map(|f| (f.path.clone(),f)).collect();
+    let mut current = Vec::new();
+
+    print!("Scanning .......... ");
 
     for entry in WalkDir::new(path){
         let entry = match entry{
@@ -73,6 +83,16 @@ fn walk(path:&str,min_size: u64,cache_file: &str) {
             continue;
         }
         files_scanned += 1;
+        let pathz = entry.path().display().to_string();
+
+        if let Some(cache_filed) = cachemap.get(&pathz) {
+            if is_file_uptodate(cache_filed) {
+                if cache_filed.size >= min_size {
+                    current.push(cache_filed.clone());
+                }
+                continue;
+            }
+        }
         let metadata = match entry.metadata() {
             Ok(m) => m,
             Err(e) => {
@@ -92,25 +112,38 @@ fn walk(path:&str,min_size: u64,cache_file: &str) {
             }
         };
 
-        if metadata.len() > min_size {
-            found_files.push((entry.path().to_path_buf(),metadata.len()));
+        if metadata.len() >= min_size {
+            if let Ok(date_modified) = get_file_time(entry.path()) {
+                let file_info = Fileinfo {
+                    path: pathz.clone(),
+                    size: metadata.len(),
+                    date_modified,
+                };
+                current.push(file_info.clone());
+                cachemap.insert(pathz, file_info);
+            }
         }
     }
+    let all_data: Vec<Fileinfo> = cachemap.into_values().collect();
+    if let Err(e) = save_cache(cache_file,&all_data) {
+        println!("Warning ! Error : {} ",e);
+    }
 
-    if found_files.is_empty() {
+    if current.is_empty() {
         println!("No files larger than {} bytes found.", min_size);
         return;
     }else {
-        println!("Found {} large files out of {} scanned:",found_files.len(),files_scanned);
-        for (path,size) in &found_files {
-            println!("  - {} ({} bytes)", path.display(), size);
+        println!("Found {} large files out of {} scanned:",current.len(),files_scanned);
+        for file in &current {
+            let date = SystemTime::UNIX_EPOCH + std::time::Duration::from_secs(file.date_modified);
+            println!("  {} ({} bytes, modified: {:?})", file.path, file.size, date);
         }
     }
     println!("===== Final Scan Statistic  =====");
     println!("Files Scanned:                {}",files_scanned);
     println!("Permission Denied:            {}",permission_denied);
     println!("Other Errors:                 {}",other_errors);
-    println!("Files Found:                  {}",found_files.len());
+    println!("Files Found:                  {}",current.len());
 }
 
 fn main() {
